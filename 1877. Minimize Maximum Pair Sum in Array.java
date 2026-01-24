ed12e7d566f2a557cdbebb1ef2d892fd3dfa5695
ADITYAchhipa <142408817+ADITYAchhipa@users.noreply.github.com>
2026-01-24 11:03:09 +0530

    Add solution for minimizing maximum pair sum in array
    Implement a solution to minimize the maximum pair sum in an array by sorting and pairing elements.

---
+++ 1877. Minimize Maximum Pair Sum in Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int max=0;
+        for(int i=0;i<n/2;i++){
+            if(nums[i]+nums[n-i-1]>max)max=nums[i]+nums[n-i-1];
+        }
+        return max;
+    }
+}
